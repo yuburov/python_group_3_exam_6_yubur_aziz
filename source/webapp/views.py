7eb2a4e5eceb15e4dcd3fe4f53d719699b09c06d
Yuburov <yubur.aziz@gmail.com>
2019-09-21 14:09:27 +0600

    Добавил базовый шаблон, также добавил главную страницу

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from webapp.models import G_book
 
-# Create your views here.
+
+def index_view(request):
+    guests = G_book.objects.filter(status__startswith='active').order_by('created_at').reverse()
+    return render(request, 'index.html', context={
+        'guests': guests
+    })
+
