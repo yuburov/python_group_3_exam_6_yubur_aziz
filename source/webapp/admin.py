9c841a8ce60f67d9f9211a3ac9944a8fec7092fa
Yuburov <yubur.aziz@gmail.com>
2019-09-21 12:35:42 +0600

    первый этапконтрольной работы

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from webapp.models import G_book
 
-# Register your models here.
+admin.site.register(G_book)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+CHOICE = 'active'
+STATUS_CHOICES = (
+    (CHOICE, 'Активно'),
+    ('blocked', 'Заблокировано'),
+)
+
+class G_book(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    email = models.EmailField(verbose_name='Почта')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
